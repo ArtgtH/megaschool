5da467ca1ef38368196b54f6372a3f4d86ec383e
artem <artem2004920@gmail.com>
2025-01-30 20:20:01 +0300

    фикс типов

--- ml/gigachad.py
+++ ml/gigachad.py
@@ -3,18 +3,14 @@ from dataclasses import dataclass
 
 @dataclass
 class Expense:
-    fuel: str = 0.0
-    maintenance: str = 0.0
-    other: str = 0.0
-    total: str = 0.0
-    overall_analysis: str = ""
+    fuel: str = "0.0"
+    maintenance: str = "0.0"
+    other: str = "0.0"
+    total: str = "0.0"
+    overall_analysis: str = "0.0"
 
 
 def test_ai(model: str, age: int, mileage: float, context: str) -> Expense:
-    return Expense(
-        fuel="1",
-        maintenance="1",
-        other="1",
-        total="1",
-        overall_analysis="1",
-    )
+    res = Expense()
+
+    return res
