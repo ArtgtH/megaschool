0067d74a52a67e2a343abb6a3ed6c466f21c106e
artem <artem2004920@gmail.com>
2025-01-30 20:15:53 +0300

    фикс

--- app.py
+++ app.py
@@ -14,12 +14,19 @@ def main():
         step=1,
     )
 
+    mileage = st.number_input("Пробег автомобиля")
+
     context = st.text_input("Дополнительная информация (например, ваш стиль вождения)")
 
     if st.button("Отправить запрос"):
-        if model:
-            res = test_ai(model, age, context)
-            st.write(f"Результат: {res}")
+        if model and context:
+            res = test_ai(model, age, mileage, context)
+            st.title("Результат")
+            st.subheader(f"РАСХОДЫ НА ТОПЛИВО: {res.fuel}")
+            st.subheader(f"РАСХОДЫ НА ТЕХ.ОБСЛУЖИВАНИЕ:: {res.maintenance}")
+            st.subheader(f"ПРОЧИЕ РАСХОДЫ: {res.other}")
+            st.subheader(f"ИТОГО: {res.other}")
+            st.subheader(f"ОБЩАЯ АНАЛИТИКА: {res.fuel}")
         else:
             st.write("Пожалуйста, введите запрос перед отправкой.")
 
